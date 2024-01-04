e35a12a4080ac18e4cc34531931da16d482f6b2c
Michael Katz <michael.katz1@ibm.com>
2024-01-04 15:22:57 -0500

    rename

--- forbiditerative/planners.py
+++ forbiditerative/planners.py
@@ -6,7 +6,9 @@ import logging
 from subprocess import SubprocessError
 from pathlib import Path
 
-def plan_unordered_topq(domain_file : str, problem_file : str, quality_bound : float, number_of_plans_bound : int = None):
+import forbiditerative
+
+def plan_unordered_topq(domain_file : Path, problem_file : Path, quality_bound : float, number_of_plans_bound : int = None) -> dict:
     build_dir = Path(forbiditerative.__file__).parent / 'builds' / 'release' / 'bin'
     build_args = ["--build", str(build_dir.absolute())]
 
@@ -14,13 +16,14 @@ def plan_unordered_topq(domain_file : str, problem_file : str, quality_bound : f
         import tempfile
         with tempfile.NamedTemporaryFile() as result_file:
             planner_args = ["--planner", "unordered_topq", "--domain", str(domain_file.absolute()), 
-                "--problem", str(problem_file.absolute()), "--quality-bound", quality_bound, "--symmetries", "--use-local-folder", "--clean-local-folder", "--suppress-planners-output", "--plans-as-json", "--results-file", str(result_file.absolute())]
+                "--problem", str(problem_file.absolute()), "--quality-bound", str(quality_bound), "--symmetries", "--use-local-folder", "--clean-local-folder", "--suppress-planners-output", "--plans-as-json", "--results-file", str(result_file.name)]
             if number_of_plans_bound:
-                planner_args.extend(["--number-of-plans", number_of_plans_bound])
+                planner_args.extend(["--number-of-plans", str(number_of_plans_bound)])
 
+            print(build_args + planner_args)
             subprocess.run([sys.executable, "-B", "-m", "forbiditerative.plan"] + build_args + planner_args)
 
-            with open(str(result_file.absolute())) as plans_file:
+            with open(str(result_file.name)) as plans_file:
                 data = json.load(plans_file)
             return data
         
